9e4ffb1c2ef31a56dfc7326bbf2c990fba29da5f
xDeFc0nx <nehartale8@gmail.com>
2022-04-22 16:19:08 +0200

    fixed navbar

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -46,9 +46,6 @@ const Header = (props) => {
         <Link href="/about">
           <a className="btn-primary">About</a>
         </Link>
-        <Link href="/contact">
-          <a className="btn-primary">Contact</a>
-        </Link>
         <Link href="https://discord.gg/eWsYZZz9Nz">
           <a className="btn-primary">Discord</a>
         </Link>
