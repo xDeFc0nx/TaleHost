ad2ff20604c1248d68c951db27a3c05f62edb69a
humayun kamal <kamalhumayun320@gmail.com>
2022-05-31 12:50:12 +0500

    Get Started Section Fixed

--- pages/index.js
+++ pages/index.js
@@ -204,96 +204,98 @@ export default function Home() {
       </div>
 
       <div className="dark:bg-salafi-light-black  dark:sm:text-white">
-      {/*Web Hosting Price */}
+        {/*Web Hosting Price */}
 
-      <div className="flex flex-col items-center sm-10 ">
-        <h1 className="heading text-center  mt-10 sm:mt-20 mb-10 w-8/12 sm:w-full dark:sm:text-white">
-          Choose Your Web Hosting Package
-        </h1>
+        <div className="flex flex-col items-center sm-10 ">
+          <h1 className="heading text-center  mt-10 sm:mt-20 mb-10 w-8/12 sm:w-full dark:sm:text-white">
+            Choose Your Web Hosting Package
+          </h1>
 
-        <div className="flex flex-wrap gap-10 justify-center ">
-          {webHosting.map((plan) => (
-            <PriceCardForWebHosting key={plan.title} {...plan} />
-          ))}
-        </div>
-      </div>
-
-      {/* Features */}
-      {/* Feature1 Extra Security */}
-      <div className="m-10 sm:m-20 dark:sm:text-white flex flex-col justify-center items-center sm:flex-row dark:sm-text-white  sm:justify-around sm:items-center">
-        <div>
-          <h1 className="heading dark:sm:text-white">Extra Security</h1>
-          <ul className="mt-6  2xl:text-2xl">
-            {[
-              "Hard Secured Servers",
-              "DDoS Protected",
-              "Up to date servers at all times",
-            ].map((feature) => (
-              <ListFeature key={feature}>{feature}</ListFeature>
+          <div className="flex flex-wrap gap-10 justify-center ">
+            {webHosting.map((plan) => (
+              <PriceCardForWebHosting key={plan.title} {...plan} />
             ))}
-          </ul>
-        </div>
-
-        <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
-          <Image className="absolute" src={muslimManInTurban} />
-        </div>
-      </div>
-
-      {/* Feature2 Simply and fast website */}
-      <div className="bg-[#F7FAFE] dark:bg-salafi-black -skew-y-6 py-1">
-        <div className="skew-y-6 m-10 sm:m-20 flex flex-col justify-center items-center sm:flex-row  sm:justify-around sm:items-center ">
-          <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
-            <Image className="absolute" src={simpleAndFastWeb} />
-  
-
           </div>
+        </div>
+
+        {/* Features */}
+        {/* Feature1 Extra Security */}
+        <div className="m-10 sm:m-20 dark:sm:text-white flex flex-col justify-center items-center sm:flex-row dark:sm-text-white  sm:justify-around sm:items-center">
           <div>
-            <h1 className="heading sm:w-8/12 dark:sm:text-white 2xl:10/12">
-              Simply and fast website
-            </h1>
+            <h1 className="heading dark:sm:text-white">Extra Security</h1>
             <ul className="mt-6  2xl:text-2xl">
-              {["1gbps internet speed at all times", "99.99% up time"].map(
-                (feature) => (
-                  <ListFeature key={feature}>{feature}</ListFeature>
-                  )
-              )}
+              {[
+                "Hard Secured Servers",
+                "DDoS Protected",
+                "Up to date servers at all times",
+              ].map((feature) => (
+                <ListFeature key={feature}>{feature}</ListFeature>
+              ))}
             </ul>
           </div>
-        </div>
-      </div>
 
-      {/* Feature3 24/7 Customer Support */}
-      <div className="m-10 sm:m-20 flex flex-col justify-center items-center sm:flex-row  sm:justify-around sm:items-center">
-        <div>
-          <h1 className="heading sm:6/12 dark:sm:text-white">24&#47;7 Customer Support</h1>
-          <ul className="mt-6  2xl:text-2xl">
-            {[
-              "24/7 Support",
-              "Average reply rate 5-25 Minutes",
-              "Discord Server",
-            ].map((feature) => (
-              <ListFeature key={feature}>{feature}</ListFeature>
+          <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
+            <Image className="absolute" src={muslimManInTurban} />
+          </div>
+        </div>
+
+        {/* Feature2 Simply and fast website */}
+        <div className="bg-[#F7FAFE] dark:bg-salafi-black -skew-y-6 py-1">
+          <div className="skew-y-6 m-10 sm:m-20 flex flex-col justify-center items-center sm:flex-row  sm:justify-around sm:items-center ">
+            <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
+              <Image className="absolute" src={simpleAndFastWeb} />
+            </div>
+            <div>
+              <h1 className="heading sm:w-8/12 dark:sm:text-white 2xl:10/12">
+                Simply and fast website
+              </h1>
+              <ul className="mt-6  2xl:text-2xl">
+                {["1gbps internet speed at all times", "99.99% up time"].map(
+                  (feature) => (
+                    <ListFeature key={feature}>{feature}</ListFeature>
+                  )
+                )}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Feature3 24/7 Customer Support */}
+        <div className="m-10 sm:m-20 flex flex-col justify-center items-center sm:flex-row  sm:justify-around sm:items-center">
+          <div>
+            <h1 className="heading sm:6/12 dark:sm:text-white">
+              24&#47;7 Customer Support
+            </h1>
+            <ul className="mt-6  2xl:text-2xl">
+              {[
+                "24/7 Support",
+                "Average reply rate 5-25 Minutes",
+                "Discord Server",
+              ].map((feature) => (
+                <ListFeature key={feature}>{feature}</ListFeature>
               ))}
-          </ul>
+            </ul>
+          </div>
+
+          <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
+            <Image className="absolute" src={supportFeature} />
+          </div>
         </div>
 
-        <div className="relative w-[15rem] sm:w-[25rem] 2xl:w-[30rem] h-[15rem] sm:h-[25rem] 2xl:h-[30rem] flex justify-center items-center">
-          <Image className="absolute" src={supportFeature} />
+        {/* Call to Action  */}
+        <div className="p-4">
+          <div className="relative bg-[#7721FF] w-[90%]  h-[22rem] sm:h-[26rem]  mx-auto  flex flex-col items-center justify-center text-center sm:text-center rounded-2xl shadow-sm">
+            <h1 className="heading !text-white w-6/12 leading-tight">
+              Start the success of your website with us
+            </h1>
+
+            <button className="py-2 px-4 mt-8 rounded-lg shadow-sm text-salafi-black bg-white  font-medium  hover:bg-salafi-grey-white ">
+              <Link href="https://billing.salafihosting.com/index.php">
+                <a>Get started</a>
+              </Link>
+            </button>
+          </div>
         </div>
-      </div>
-
-      {/* Call to Action  */}
-      <div className="relative bg-[#7721FF] w-[90%] h-[26rem] p-2 sm:p-0 mx-auto  flex flex-col items-center justify-center text-center sm:text-center rounded-2xl shadow-sm">
-        <h1 className="heading !text-white w-6/12 leading-tight">
-          Start the success of your website with us
-        </h1>
-
-        <button className="py-2 px-4 mt-8 rounded-lg shadow-sm text-salafi-black bg-white  font-medium  hover:bg-salafi-grey-white ">
-          <Link href="https://billing.salafihosting.com/index.php">
-            <a>Get started</a>
-          </Link>
-        </button>
-              </div>
 
         {/* svg1 */}
 
