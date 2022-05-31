4d3bc2c72212475ec988c77da648581e33709355
humayun kamal <kamalhumayun320@gmail.com>
2022-05-31 12:30:05 +0500

    Dark fixed on Mobile

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -11,11 +11,9 @@ const Header = (props) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const openDrawerHandler = () => {
     setDrawerIsOpen(true);
-    console.log(drawerIsOpen);
   };
   const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
-    console.log(drawerIsOpen);
   };
 
   const toggleDarkMode = () => {
@@ -29,7 +27,6 @@ const Header = (props) => {
       localStorage.setItem("theme", "dark");
     }
     setIsDarkMode((state) => !state);
-    console.log(isDarkMode);
   };
   function MyLink(props) {
     let { href, children, ...rest } = props;
@@ -178,15 +175,48 @@ const Header = (props) => {
             </a>
           </Link>
         </div>
-        <div className="block md:hidden group">
-          <button
-            className="w-[2.5rem] h-[2.6rem] bg-transparent   flex flex-col justify-around cursor-pointer"
-            onClick={openDrawerHandler}
-          >
-            <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn  group-active:rotate-45 transition-transform origin-top-left"></span>
-            <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn group-hover:translate-x-2 transition-transform group-active:opacity-0"></span>
-            <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn  group-active:-rotate-45 transition-transform origin-top-left"></span>
-          </button>
+
+        <div className="flex justify-center items-center">
+          <div className="flex flex-row justify-center md:hidden items-center">
+            {isDarkMode && (
+              <div className="mr-10" onClick={toggleDarkMode}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            )}
+            {!isDarkMode && (
+              <div className="mr-10" onClick={toggleDarkMode}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-10 w-10"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                </svg>
+              </div>
+            )}
+          </div>
+          <div className="block md:hidden group">
+            <button
+              className="w-[2.5rem] h-[2.6rem] bg-transparent   flex flex-col justify-around cursor-pointer"
+              onClick={openDrawerHandler}
+            >
+              <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn  group-active:rotate-45 transition-transform origin-top-left"></span>
+              <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn group-hover:translate-x-2 transition-transform group-active:opacity-0"></span>
+              <span className="block w-[2.5rem] h-[2px] bg-salafi-primary-btn  group-active:-rotate-45 transition-transform origin-top-left"></span>
+            </button>
+          </div>
         </div>
       </header>
     </Fragment>
