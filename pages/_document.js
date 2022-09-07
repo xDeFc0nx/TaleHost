9c0abaff4a7e4a9e925bbb360c6134662fb5bbb5
humayun <kamalhumayun320@gmail.com>
2022-09-07 17:05:43 +0500

    chat bot add

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script
+          src="//code.tidio.co/r0drkzxogrdystb1n9njx9n1fp5gwt7q.js"
+          async
+        ></script>
+      </body>
+    </Html>
+  );
+}
